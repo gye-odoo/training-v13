82d5382b0264741274a432eb703b348f8c25bef8
gye-odoo <44210408+gye-odoo@users.noreply.github.com>
2019-10-21 21:36:31 +0000

    First commit

---
+++ technical-training-13.0-01-models/library/models/book.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields, _
+
+class Book(models.Model):
+    
+    _name = "library.book"
+    
+    name = fields.Text(required=True)
+    isbn = fields.Text(required=True)
+    edition_date = fields.Date()
+    rented_by = fields.One2many("library.customer", "book_id")
+    
---
+++ technical-training-13.0-01-models/library/models/customer.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields, _
+
+class Customer(models.Model):
+    
+    _name = "library.customer"
+    
+    name = fields.Text(required=True, string="Customer Name")
+    book_id = fields.Many2one("library.book")
